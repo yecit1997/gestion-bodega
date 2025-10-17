fa2cc47cc000848ccf781253d0c5595bb34764c8
yecit1997 <ttorresyecit@gmail.com>
2025-10-16 23:01:57 -0500

    Creamos las vistas y serializers para las categorias

--- apps/categoria/urls.py
+++ apps/categoria/urls.py
@@ -1,3 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 from .views import CategoriaViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'categorias', CategoriaViewSet, basename='categoria')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
