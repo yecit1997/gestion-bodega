aa863e8592abf2e866a0c003ef510df3102e04df
yecit1997 <ttorresyecit@gmail.com>
2025-10-17 12:21:45 -0500

    Configuramos una DB Postgre y migraos la información a esta

--- apps/proveedor/views.py
+++ apps/proveedor/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+# from rest_framework.permissions import IsAuthenticated
+from .models import Proveedor
+from .serializers import ProveedorSerializer
 
-# Create your views here.
+class ProveedorViewSet(viewsets.ModelViewSet):
+    queryset = Proveedor.objects.all()
+    serializer_class = ProveedorSerializer
+    # permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
--- core/settings.py
+++ core/settings.py
@@ -77,24 +77,29 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "bodega_db",
-#         "USER": "root",
-#         "PASSWORD": "bodega_db",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     },
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bodega_db",
+        "USER": "postgres",
+        "PASSWORD": "Yecit19970730",
+        "HOST": "localhost",
+        "PORT": "5432",
+        "OPTIONS": { # Agrega la sección OPTIONS
+            "client_encoding": "UTF8",
+            # Otras opciones si las necesitas:
+            # "options": "-c default_transaction_isolation=read committed",
+            # "options": "-c timezone=UTC",
+        },
+    },
+}
 
 
 # Password validation
