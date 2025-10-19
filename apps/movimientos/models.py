7a522275abee7881b0da8f5466690d4d3327ac09
yecit1997 <ttorresyecit@gmail.com>
2025-10-19 14:31:47 -0500

    Configuramos el origen en la tabla movimientos para los elementos o productos que cuenta con un stock desde su creación

--- apps/movimientos/models.py
+++ apps/movimientos/models.py
@@ -4,17 +4,28 @@ from apps.producto.models import Producto
 import uuid
 
 class Movimiento(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Aqui manejamos las opciones de tipo de movimiento
     ENTRADA = 'E'
     SALIDA = 'S'
     TIPO_CHOICES = [
         (ENTRADA, 'Entrada'),
         (SALIDA, 'Salida'),
     ]
+    
+    # 
+    MANUAL = 'M'
+    AUTOMATICO = 'A'
+    ORIGEN_CHOICES = [
+        (MANUAL, 'Manual'),
+        (AUTOMATICO, 'Automático'),
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     tipo = models.CharField(max_length=1, choices=TIPO_CHOICES)
     cantidad = models.PositiveIntegerField()
+    origen = models.CharField(max_length=20, choices=ORIGEN_CHOICES, default=MANUAL)
     fecha = models.DateTimeField(auto_now_add=True)
     observaciones = models.TextField(blank=True)
 
--- apps/producto/serializers.py
+++ apps/producto/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
+from django.db import transaction
 from .models import Producto
+from apps.movimientos.models import Movimiento
 
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = '__all__'
+        
+    @transaction.atomic
+    def create(self, validated_data):
+        stockt_inicial = validated_data.get('stock', 0)
+        producto = super().create(validated_data)
+        
+        # Si el producto se crea con stock inicial mayor a 0, crear un movimiento de entrada
+        if stockt_inicial > 0:
+            Movimiento.objects.create(
+                producto=producto,
+                tipo=Movimiento.ENTRADA,
+                cantidad=stockt_inicial,
+                origen=Movimiento.AUTOMATICO,
+                observaciones="Registro inicial del producto."
+            )
+       
+        return producto
